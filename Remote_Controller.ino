63c125a947217998fe1cb07f98b7e3fa4b58844e
ajr426 <ajr426@gmail.com>
2020-03-04 14:31:49 -0600

    Integrated API mode into remote controller (using joysticks)

--- Remote_Controller.ino
+++ Remote_Controller.ino
@@ -4,6 +4,23 @@
 
 #include <LiquidCrystal.h>
 
+#include <XBee.h>
+
+/*
+This example is for Series 2 XBee
+ Sends a ZB TX request with the value of analogRead(pin5) and checks the status response for success
+*/
+
+// create the XBee object
+XBee xbee = XBee();
+
+uint8_t payload[] = { 0, 0 };
+
+// SH + SL Address of receiving XBee
+XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40b40397);
+ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+
 //LCD pins
 //RS pin = pin 12
 //Enable pin = pin 11
@@ -52,6 +69,8 @@ remote_data raw_data;       //raw data read in from joystick
 void setup() {
   // initialize both serial ports:
   Serial.begin(9600);  //intitializes communication with xbee pro s2b
+  xbee.setSerial(Serial); //sets Serial to xbee library
+
 
   //LCD screen setup
   // set up the LCD's number of columns and rows:
@@ -76,13 +95,13 @@ void loop()
   //change 0 to 1023 range to 0 to 254 range
   //xbee sends byte (0 to 255 range) wirelessly
   //we reserve 255 to declare the start of a data packet
-  output_data.jx = map(raw_data.jx, 0, 1023, 0, 254);
-  output_data.jy = map(raw_data.jy, 0, 1023, 0, 254);
+  payload[0] = map(raw_data.jx, 0, 1023, 0, 254);
+  payload[1] = map(raw_data.jy, 0, 1023, 0, 254);
 
   //determines if joystick position has changed
   //Joystick position is only sent to robot if it has changed
-  diffx = abs(output_data.jx - prev_data.jx);
-  diffy = abs(output_data.jy - prev_data.jy);
+  diffx = abs(raw_data.jx - prev_data.jx);
+  diffy = abs(raw_data.jy - prev_data.jy);
 
   //If the difference is bigger than 10, then send data to robot
   if(diffx > 10 || diffy > 10)
@@ -103,21 +122,40 @@ void loop()
   //xbee then transmits that data wirelessly
   if (send_data)
   {
-    Serial.write(255);
-    delay(20);                     //delays used to make sure xbee module has time to process data
-    Serial.write(output_data.jx);
-    delay(20);
-    Serial.write(output_data.jy);
-    delay(20);
-    prev_data.jx = output_data.jx;   //used to calculate whether joystick position has changed
-    prev_data.jy = output_data.jy;
+    xbee.send(zbTx);
     digitalWrite(LED_BUILTIN,HIGH);  //indicates if joystick has been moved
     lcd.clear();
     lcd.print("Sent x:");
-    lcd.print(output_data.jx);
+    lcd.print(payload[0]);
     lcd.setCursor(0,1);
     lcd.print("Sent y:");
-    lcd.print(output_data.jy);
+    lcd.print(payload[1]);
+    // after sending a tx request, we expect a status response
+    // wait up to half second for the status response
+    if (xbee.readPacket(500)) {
+      // got a response!
+
+      // should be a znet tx status
+      if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
+        xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+        // get the delivery status, the fifth byte
+        if (txStatus.getDeliveryStatus() == SUCCESS) {
+          // success.  time to celebrate
+
+        } else {
+          // the remote XBee did not receive our packet. is it powered on?
+
+        }
+      }
+    } else if (xbee.getResponse().isError()) {
+      //nss.print("Error reading packet.  Error code: ");
+      //nss.println(xbee.getResponse().getErrorCode());
+    } else {
+      // local XBee did not provide a timely TX Status Response -- should not happen
+
+    }
+
   }
 
   delay(20);          //makes program run better
